f83ecd2d7df383ea516530739cef6c910ee30bea
AMereBagatelle <themerebagatelle@gmail.com>
2021-02-04 11:31:37 -0500

    Initial Commit

---
+++ src/main/java/io/github/amerebagatelle/disabler/Disabler.java
@@ -0,0 +1,10 @@
+package io.github.amerebagatelle.disabler;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Disabler implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
